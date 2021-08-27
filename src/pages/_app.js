0720811d64d512714185991e02fe437381b126c7
SqueezyDough <lvanbiljouw@hotmail.com>
2021-08-27 13:50:05 +0200

    refactor: head element to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from '../styles/global'
 import Theme from '../styles/theme'
@@ -5,6 +6,12 @@ import Theme from '../styles/theme'
 function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>3D Gallery</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <ThemeProvider theme={Theme}>
         <GlobalStyle />
         <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>3D Gallery</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>3D Gallery</h1>
-    </div>
+    <>
+      <main>
+        {/* TODO: Build Main components */}
+      </main>
+
+      <footer>
+        {/* TODO: Build footer component */}
+      </footer>
+    </>
   )
 }
